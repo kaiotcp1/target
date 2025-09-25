22da83de91ebca4ec1603f68cbb9c598fa3c0714
kaiotcp1 <kaiotcp1@gmail.com>
2025-09-25 14:18:16 -0300

    feat: add in-progress bar component and page

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,11 +1,28 @@
-import { Button, Text, View } from "react-native";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
 import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+const details = {
+  current: 'R$ 580,00',
+  target: 'R$ 1.790,00',
+  percentage: 25
+}
 
 export default function InProgress() {
 
   return (
-    <View>
-      <Text>Em progresso</Text>
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => { }
+        }}
+      />
+      <Progress
+        data={details}
+      />
       <Button title="Voltar" onPress={() => router.back()} />"
     </View>
   )
