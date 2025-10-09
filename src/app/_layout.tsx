60d3bd7a9fe624032cbd1624549ba39e47d0f68e
kaiotcp1 <kaiotcp1@gmail.com>
2025-10-09 17:44:07 -0300

    feat: add sqlite config and first method

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from 'react'
 import { colors } from '../theme/colors'
 import { Stack } from 'expo-router'
 import {
   useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold
 } from '@expo-google-fonts/inter'
 import { Loading } from '../components/Loading'
+
+import { SQLiteProvider } from 'expo-sqlite'
+
+import { migrate } from '@/database/migrate'
 
 export default function Layout() {
   const [fonstLoaded] = useFonts({
@@ -16,10 +21,20 @@ export default function Layout() {
     return <Loading />
   }
 
-  return <Stack
-    screenOptions={{
-      headerShown: false,
-      contentStyle: { backgroundColor: colors.white }
-    }}
-  />
+  return (
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider
+        databaseName='target.db'
+        onInit={migrate}
+        useSuspense={true}
+      >
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white }
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
+  )
 }
--- src/app/target.tsx
+++ src/app/target.tsx
@@ -2,9 +2,48 @@ import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
-import { View, Text } from "react-native";
+import { useTargetDatabase } from "@/database/useTargetDatabase";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import { View, Text, Alert } from "react-native";
 
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const params = useLocalSearchParams<{ id?: string }>()
+  const targetDatabase = useTargetDatabase()
+
+  function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert('Atenção', 'Informe o nome e o valor da meta.');
+    }
+    setIsProcessing(true);
+
+    if (params.id) {
+
+    } else {
+      create()
+    }
+  }
+
+  async function create() {
+    try {
+      await targetDatabase.create({ name, amount })
+      Alert.alert('Sucesso', 'Meta criada com sucesso.', [
+        {
+          text: 'Ok', onPress: () => router.back()
+        }
+      ]);
+    } catch (error: any) {
+      Alert.alert('Erro', 'Não foi possível criar a meta.');
+      console.log(error)
+      setIsProcessing(false);
+    }
+  }
+
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
@@ -15,12 +54,15 @@ export default function Target() {
         <Input
           label="Nome da meta"
           placeholder="Ex: Viagem para praia, Apple Watch"
+          value={name}
+          onChangeText={setName}
         />
         <CurrencyInput
           label="Valor alvo (R$)"
-          value={0}
+          value={amount}
+          onChangeValue={setAmount}
         />
-        <Button title="Salvar" />
+        <Button title="Salvar" isProcessing={isProcessing} onPress={handleSave} />
       </View>
     </View >
   )
